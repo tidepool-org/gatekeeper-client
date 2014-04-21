6a91ac1a65ccb8f85b4dc48d4f9b953fc02a190b
Eric Tschetter <cheddar@tidepool.org>
2014-04-21 13:57:48 -0500

    Client to talk to gatekeeper

---
+++ test/testClient.js
@@ -0,0 +1,177 @@
+/*
+ * == BSD2 LICENSE ==
+ */
+
+var salinity = require('salinity');
+var expect = salinity.expect;
+var sinon = salinity.sinon;
+
+describe('client.js', function() {
+  var token;
+  var hosts;
+  var request = sinon.stub();
+
+  var httpClient = require('amoeba').httpClient({}, request);
+  var tokenGetter = function(){ return token; };
+  var hostGetter = { get: function(){ return hosts; } };
+
+  var client;
+
+  beforeEach(function() {
+    token = '1234';
+    hosts = [{ protocol: 'http', host: 'localhost:1234' }];
+    request.reset();
+
+    client = require('../lib').client(httpClient, tokenGetter, hostGetter);
+  });
+
+  describe('userInGroup', function() {
+    it('does a request', function(done) {
+      request.callsArgWith(1, null, { statusCode: 200 }, '{ "view": {} }');
+
+      client.userInGroup('1234', 'abcd', function(err, result) {
+        expect(result).to.deep.equal({ view: {} });
+        expect(request).calledWith(
+          sinon.match({
+            url: 'http://localhost:1234/authorized/abcd/1234',
+            method: 'GET',
+            headers: { 'x-tidepool-session-token': '1234'}
+          }),
+          sinon.match.func
+        );
+        done(err);
+      });
+    });
+
+    it('passes back error codes', function(done) {
+      request.callsArgWith(1, null, { statusCode: 401 }, 'billybobbump');
+
+      client.userInGroup('1234', 'abcd', function(err, result) {
+        expect(result).to.not.exist;
+        expect(err).to.deep.equal({ statusCode: 401, message: 'billybobbump' });
+        expect(request).calledWith(
+          sinon.match({
+                        url: 'http://localhost:1234/authorized/abcd/1234',
+                        method: 'GET',
+                        headers: { 'x-tidepool-session-token': '1234'}
+                      }),
+          sinon.match.func
+        );
+        done();
+      });
+    });
+  });
+
+  describe('usersInGroup', function() {
+    it('does a request', function(done) {
+      request.callsArgWith(1, null, { statusCode: 200 }, '{ "view": {} }');
+
+      client.usersInGroup('abcd', function(err, result) {
+        expect(result).to.deep.equal({ view: {} });
+        expect(request).calledWith(
+          sinon.match({
+                        url: 'http://localhost:1234/authorized/abcd',
+                        method: 'GET',
+                        headers: { 'x-tidepool-session-token': '1234'}
+                      }),
+          sinon.match.func
+        );
+        done(err);
+      });
+    });
+
+    it('passes back error codes', function(done) {
+      request.callsArgWith(1, null, { statusCode: 401 }, 'billybobbump');
+
+      client.usersInGroup('abcd', function(err, result) {
+        expect(result).to.not.exist;
+        expect(err).to.deep.equal({ statusCode: 401, message: 'billybobbump' });
+        expect(request).calledWith(
+          sinon.match({
+                        url: 'http://localhost:1234/authorized/abcd',
+                        method: 'GET',
+                        headers: { 'x-tidepool-session-token': '1234'}
+                      }),
+          sinon.match.func
+        );
+        done();
+      });
+    });
+  });
+
+  describe('groupsForUser', function() {
+    it('does a request', function(done) {
+      request.callsArgWith(1, null, { statusCode: 200 }, '{ "view": {} }');
+
+      client.groupsForUser('1234', function(err, result) {
+        expect(result).to.deep.equal({ view: {} });
+        expect(request).calledWith(
+          sinon.match({
+                        url: 'http://localhost:1234/authorized/group/1234',
+                        method: 'GET',
+                        headers: { 'x-tidepool-session-token': '1234'}
+                      }),
+          sinon.match.func
+        );
+        done(err);
+      });
+    });
+
+    it('passes back error codes', function(done) {
+      request.callsArgWith(1, null, { statusCode: 401 }, 'billybobbump');
+
+      client.groupsForUser('1234', function(err, result) {
+        expect(result).to.not.exist;
+        expect(err).to.deep.equal({ statusCode: 401, message: 'billybobbump' });
+        expect(request).calledWith(
+          sinon.match({
+                        url: 'http://localhost:1234/authorized/group/1234',
+                        method: 'GET',
+                        headers: { 'x-tidepool-session-token': '1234'}
+                      }),
+          sinon.match.func
+        );
+        done();
+      });
+    });
+  });
+
+  describe('setPermissions', function() {
+    it('does a request', function(done) {
+      request.callsArgWith(1, null, { statusCode: 200 }, '{ "view": {} }');
+
+      client.setPermissions('1234', 'abcd', { view: {} }, function(err, result) {
+        expect(result).to.deep.equal({ view: {} });
+        expect(request).calledWith(
+          sinon.match({
+                        url: 'http://localhost:1234/authorized/abcd/1234',
+                        method: 'POST',
+                        headers: { 'x-tidepool-session-token': '1234', 'content-type': 'application/json' },
+                        body: JSON.stringify({ view: {} })
+                      }),
+          sinon.match.func
+        );
+        done(err);
+      });
+    });
+
+    it('passes back error codes', function(done) {
+      request.callsArgWith(1, null, { statusCode: 401 }, 'billybobbump');
+
+      client.setPermissions('1234', 'abcd', { view: {} }, function(err, result) {
+        expect(result).to.not.exist;
+        expect(err).to.deep.equal({ statusCode: 401, message: 'billybobbump' });
+        expect(request).calledWith(
+          sinon.match({
+                        url: 'http://localhost:1234/authorized/abcd/1234',
+                        method: 'POST',
+                        headers: { 'x-tidepool-session-token': '1234', 'content-type': 'application/json' },
+                        body: JSON.stringify({ view: {} })
+                      }),
+          sinon.match.func
+        );
+        done();
+      });
+    });
+  });
+});
